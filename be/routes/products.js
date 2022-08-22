3c36cd6796b108e2fd44d4f8e751ec5500ed29c0
Ciara Sims <ciarasims08@gmail.com>
2022-08-21 21:03:53 -0400

    code test

--- be/routes/products.js
+++ be/routes/products.js
@@ -1,9 +1,47 @@
-const express = require('express');
-const data = require('../data/products');
+const express = require("express");
+const products = require("../data/products");
+const data = require("../data/products");
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('Gonna start here')
+// 1. return all products
+router.get("/", function (req, res, next) {
+  res.send(data);
+});
+
+// score for characteristics
+const scores = {
+  "Humane": 1,
+  "Locally Produced": 1,
+  "Healthy": 1,
+  "Plastic-Free": 2,
+  "Unhealthy": -1,
+  "Wasteful": -1,
+};
+
+// function to return score for product
+function getScore(characteristics) {
+  let score = 0;
+  for (characteristic in characteristics) {
+    score += scores[characteristics[characteristic]] || 0;
+  }
+  return score;
+}
+
+// 3. return products with associated score
+router.get("/score", function (req, res, next) {
+  let products = data;
+  for (const product in products) {
+    products[product].score = getScore(products[product].characteristics);
+  }
+  res.send(products);
+});
+
+// 2. return products with given characteristic
+router.get("/:characteristic", function (req, res, next) {
+  const products = data.filter((data) =>
+    data.characteristics.includes(req.params.characteristic)
+  );
+  res.send(products);
 });
 
 module.exports = router;
--- fe/src/App.js
+++ fe/src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useState } from "react";
+import "./App.css";
 
 function App() {
+  const characteristics = [
+    "Wasteful",
+    "Plastic-Free",
+    "Locally Produced",
+    "Healthy",
+    "Humane",
+    "Unhealthy",
+    "Vegan",
+  ];
+
+  // create array for checkbox state
+  const [checkedState, setCheckedState] = useState(
+    new Array(characteristics.length).fill(false)
+  );
+
+  // create object for state of product data
+  const initialProductsState = {};
+  const [products, updateProduct] = useReducer(
+    (state, updates) => ({ ...state, ...updates }),
+    initialProductsState
+  );
+
+  // function to remove product based on checkbox state
+  const RemoveProduct = characteristic => useReducer(
+    (state, updates) => ({...state, ...updates}),
+    delete products[characteristic],
+    products
+  )
+
+  // handle checkbox selection
+  const handleOnChange = (characteristic, position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+
+    checkedState[position]
+      ? RemoveProduct(characteristic)
+      : getProducts(characteristic).then((data) => {
+          const product = data.map((x) => x.name);
+          updateProduct({ [characteristic]: product });
+        });
+  };
+
+  const showProducts = products.length || checkedState.includes(true);
+
+  const getProducts = async (characteristic) => {
+    const result = await fetch(
+      `http://localhost:8000/products/${characteristic}`
+    );
+    const data = result.json();
+    return data;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="selectBoxTitle">
+        <h2>Characteristics</h2>
+        <p>Choose characteristics to see matching products</p>
+        <div className="selectBox">
+          {characteristics.map((option, index) => (
+            <div key={index}>
+              <input
+                type="checkbox"
+                checked={checkedState[index]}
+                name={option}
+                id={index}
+                value={option}
+                onChange={() => handleOnChange(option, index)}
+              />
+              {option}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        {showProducts ? (
+          <div className="productList">
+            <h1 className="productTitle">Products</h1>
+            <div className="products">
+              {Object.entries(products).map(([key, value]) => (
+                <div key={key}>
+                  <h4>{key}</h4>
+                  {products[key].map((item, index) => (
+                    <ul key={index}>{item}</ul>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
